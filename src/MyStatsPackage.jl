aab2bc051ba890bbe1106be59f3fa5f4c6c4a95d
ElViIsMe <lukas.vosse@inspo.uni-stuttgart.de>
2023-10-10 10:15:35 +0200

    First commit

---
+++ src/MyStatsPackage.jl
@@ -0,0 +1,9 @@
+module MyStatsPackage
+
+using ProgressMeter
+include("c:/Users/LV/Documents/JuliaRSESuSc/statistic_functions.jl")
+
+export rse_sum
+export rse_mean, rse_tstat
+
+end # module MyStatsPackage
