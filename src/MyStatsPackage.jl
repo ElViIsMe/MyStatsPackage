fd765bf46c370f26a08af83e600f3a3da8634682
ElViIsMe <lukas.vosse@inspo.uni-stuttgart.de>
2023-10-10 11:31:06 +0200

    Upd

--- src/MyStatsPackage.jl
+++ src/MyStatsPackage.jl
@@ -2,8 +2,10 @@ module MyStatsPackage
 
 using ProgressMeter
 include("c:/Users/LV/Documents/JuliaRSESuSc/statistic_functions.jl")
+include("printOwner.jl")
 
 export rse_sum
-export rse_mean, rse_tstat
+export rse_mean, rse_tstat, printOwner
 
-end # module MyStatsPackage
+end # module MyStatsPackage https://github.com/ElViIsMe/MyStatsPackage
+#add https://github.com/ElViIsMe/nwietersMyStatsPackage
